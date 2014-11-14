270609381fceda80724765834e51e05cf7259588
RiseShia <rise.shia@gmail.com>
2014-11-14 13:34:23 +0900

    Code optimization

--- 3n+1/neocoin_riseshia/code.rb
+++ 3n+1/neocoin_riseshia/code.rb
@@ -1,40 +1,68 @@
-def n3_1(n,dic)
+$dic = {1 => 1}
+
+def n3_1(n)
   count = 1
+  route = [n]
 
-  # print "#{n},"
   while(n != 1)
-    count += 1
-
-    if n.even?
-      n /= 2
+    if n & 1 == 0
+      n = n >> 1
     else
-      n = n+n+n+1
-    end
-    
-    # print "#{n}," 
-    if dic.key?(n)
-      return count + dic[n] -1
+      n = 3*n + 1
     end
 
-  end  
+    # puts "#{n}, #{count}"
+    if $dic.key?(n)
+      count = count + $dic[n] - 1
+      break
+    else
+      route << n
+      count += 1
+    end
+  end
+  count += 1 # for 1
+
+  # puts "result #{count}"
+  temp = count
+  route.each do |el|
+    $dic[el] = temp
+    temp -= 1
+  end
 
   count
 end
 
 def test(i,j)
   max = 0
-  dic = {}
 
-  (i..j).each do |d|
-    count = n3_1(d,dic)
-    dic[d] = count
+  # Swap variable if i is small
+  if i > j
+    i,j = j,i
+  end
 
-    max = count if count > max
+  # check
+  d = i
+  last = j+1
+  while(d < last)
+    unless $dic.key?(d)
+      count = n3_1(d)
+      max = count if count > max
+    end
 
-    # puts "=>#{d}, #{count}"
+    if $dic.key?(d+1)
+      d += 2
+      if $dic.key?(d+2)
+        d += 3
+        if $dic.key?(d+3)
+          d += 4
+        end
+      end
+    else
+      d += 1
+    end
   end
   
-  puts max
+  puts max  
 end
 
 def test2(i,j)
@@ -46,39 +74,10 @@ def test2(i,j)
   pp intermediate.sort{|l,r| r[1] <=> l[1]}[0]
 end
 
+test(1,1_000_000)
 
-if __FILE__ == $0
-  test(1, 10_000_000)
-end
+# 525
 
-# if __FILE__ == $0
-#   # test(1, 22)
-#   test(1, 22)
-# end
-
-
-# [525]
-
-# 1_000_000 try 1 even?
-# real  0m13.855s
-# user  0m13.759s
-# sys 0m0.073s
-
-# 1_000_000 try 2 even? -> %
-# real  0m15.263s
-# user  0m15.075s
-# sys 0m0.123
-
-# 1_000_000 try 3 dic
-# real  0m3.435s
-# user  0m3.383s
-# sys 0m0.042s
-
-
-# 686
-
-# real  0m42.834s
-# user  0m42.340s
-# sys 0m0.421s
-
-
+# real  0m2.553s
+# user  0m2.466s
+# sys 0m0.072s
