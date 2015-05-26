51ca5c2748aca8f09b065838b6eb37fe0aeef186
ChangHoon Jeong <iamseapy@gmail.com>
2015-05-26 21:05:32 +0900

    반복문을 루비 권장스타일로 변경

--- josephus/asanadui_seapy/josephus.rb
+++ josephus/asanadui_seapy/josephus.rb
@@ -2,7 +2,10 @@
 class Josephus
   def survivor(n:, k:)
     list = (1..n).to_a
-    list.rotate!(k-1).shift while list.length > 1
+    loop do
+      list.rotate!(k-1).shift
+      break if list.length == 1
+    end
     list.first
   end
 end
