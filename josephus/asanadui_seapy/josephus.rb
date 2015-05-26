4a48e45a0a9498e24706229d150f31d13355051a
ChangHoon Jeong <iamseapy@gmail.com>
2015-05-26 21:02:37 +0900

    반복문 수정

--- josephus/asanadui_seapy/josephus.rb
+++ josephus/asanadui_seapy/josephus.rb
@@ -2,12 +2,7 @@
 class Josephus
   def survivor(n:, k:)
     list = (1..n).to_a
-    loop do
-      list.rotate!(k-1).shift
-      if list.length == 1
-        break
-      end
-    end
+    list.rotate!(k-1).shift while list.length > 1
     list.first
   end
 end
