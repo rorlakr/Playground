0a8d951eee56a59b9d99945e72723b1bab116170
Shia <rise.shia@gmail.com>
2015-06-03 19:36:40 +0900

    change code based on module

--- josephus/shia/josephus.rb
+++ josephus/shia/josephus.rb
@@ -1,27 +1,33 @@
-class Josephus
-  def self.round(k)
-    i = (@@list.size > k ? @@offset % @@list.size : @@offset)
+module Josephus
+  class << self
+    attr_accessor :list, :offset
+  end
+
+  def round(k)
+    i = (@list.size > k ? @offset % @list.size : @offset)
     result = []
 
-    @@offset = k - 1 - (@@list.size - @@offset -1) % k 
-    result << @@list.delete_at(i)
-    (i+k-1).step(@@list.size, k-1) do |d|
-      break unless @@list.size > d
-      result << @@list.delete_at(d)
+    @offset = k - 1 - (@list.size - @offset - 1) % k 
+    result << @list.delete_at(i)
+    (i+k-1).step(@list.size, k-1) do |d|
+      break unless @list.size > d
+      result << @list.delete_at(d)
     end
     true
   end
 
-  def self.full(n,k)
-    @@list = (1..n).to_a
-    @@offset = k-1
-    round(k) until @@list.size < 2
+  def full(n,k)
+    @list = (1..n).to_a
+    @offset = k-1
+    round(k) until @list.size < 2
   end
 
-  def self.surviver
-    @@list.first
+  def surviver
+    @list.first
   end
+
+  module_function :round, :full, :surviver
 end
 
-Josephus.full(100_000,1000)
-p Josephus.surviver
+Josephus.full(10,3)
+p Josephus.surviver == 4
